f963978d048d75dd03b71a7b2330c6a499e0370e
Samantha Hughes <samantha.patricia.hughes2@gmail.com>
2022-05-24 11:52:52 -0400

    part1

--- TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechJobsPersistent.Models;
+
 namespace TechJobsPersistent.ViewModels
 {
     public class AddEmployerViewModel
     {
-        public AddEmployerViewModel()
-        {
-        }
+       
     }
 }
--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,5 +1,11 @@
 --Part 1
 
+Jobs Table:
+Id : INT
+Name : TEXT
+EmployerId : INT
+
+
 --Part 2
 
 --Part 3
